3ec16c003bdf0f3cc477c7c983cce3e7b2ed4b2f
affjljoo3581 <affjljoo3581@gmail.com>
2019-08-10 20:19:32 +0900

    change

--- src/run_base_model.py
+++ src/run_base_model.py
@@ -24,8 +24,22 @@ flags.DEFINE_float('min_lr', 1e-5, 'minimum learning rate')
 flags.DEFINE_bool('use_fp16', True, 'use half-precision to decrease training time and model capacity')
 flags.DEFINE_float('loss_scale', 128, 'loss scaling factor to preserve small gradient magnitudes')
 
-NUM_IMAGENET_TRAIN_IMAGES = 1281167
-flags.DEFINE_integer('total_iters', 20 * NUM_IMAGENET_TRAIN_IMAGES // 64, 'number of total iterations for training')
+flags.DEFINE_integer('total_iters', 20 * 1281167 // 64, 'number of total iterations for training')
 flags.DEFINE_integer('log_iters', 100, 'number of iterations to test model and log training results')
 flags.DEFINE_integer('save_iters', 5000, 'number of iterations to save model weights')
 
+
+def main(argv):
+    model_fn = lambda x, is_training: modeling.ResNet50(x, FLAGS.classes, is_training)
+
+    training.train(FLAGS, model_fn)
+    tf.reset_default_graph()
+    metrics = training.evaluate(FLAGS, model_fn)
+    
+    with open(FLAGS.log_path, 'a') as fp:
+        fp.write(str(metrics))
+    
+    os.system('sudo poweroff')
+        
+if __name__ == '__main__':
+    app.run(main)
